5db9e95019981692288486cdaba12017eab7d6fe
Matt Brooks <115656591+mattbrooks100@users.noreply.github.com>
2022-12-20 12:07:35 -0500

    added delete entry feature

--- public/app.js
+++ public/app.js
@@ -6,14 +6,26 @@ fetch("/api/entries")
   .then((entryArray) => {
     console.log(entryArray);
     for (let entry of entryArray) {
-      const { entry_date, wake_feel, accomplish, main_entry, tomorrow } = entry;
+      const { id, entry_date, wake_feel, accomplish, main_entry, tomorrow } = entry;
       const newEntryDiv = document.createElement("div");
-      newEntryDiv.className = "entry mb-4 p-4 bg-amber-100 shadow-lg rounded-xl";
+      newEntryDiv.className = "entry relative mb-4 p-4 bg-amber-100 shadow-lg rounded-xl";
+      newEntryDiv.setAttribute("id", `${id}`);
       newEntryDiv.innerHTML = `<b>Date:</b> ${entry_date}<br>
-        <b>How did you feel when you woke up?</b> ${wake_feel}<br>
-        <b>What did you accomplish?</b> ${accomplish}<br>
-        <b>What happened today?</b> ${main_entry}<br>
-        <b>What will you accomplish tomorrow?</b> ${tomorrow}`;
+        <b>How did you feel when you woke up?</b> <p>${wake_feel}</p>
+        <b>What did you accomplish?</b> <p>${accomplish}</p>
+        <b>What happened today?</b> <p>${main_entry}</p>
+        <b>What will you accomplish tomorrow?</b> <p>${tomorrow}</p>`;
+      const deleteButton = document.createElement("button");
+      deleteButton.innerText = "Delete entry";
+      deleteButton.className = "delete-button absolute top-0 right-0 mt-4 mr-4 text-purple-800 hover:text-black";
+      deleteButton.onclick = () => {
+        const id = deleteButton.parentElement.getAttribute("id");
+        fetch(`/api/entries/${id}`, {
+          method: "DELETE",
+        });
+        location.reload();
+      };
+      newEntryDiv.append(deleteButton);
       entryContainer.append(newEntryDiv);
     }
   });
@@ -25,27 +37,32 @@ const closeButton = document.querySelector("#close-button");
 newEntryButton.onclick = () => (entryModal.style.display = "block");
 closeButton.onclick = () => (entryModal.style.display = "none");
 
-// Submit a new entry
+// Submit a new entry and POST request
 const entryForm = document.querySelector("#entry-form");
 const submitButton = document.querySelector("#submit-button");
 const verification = document.querySelector("#verification");
 entryForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const data = new FormData(e.target);
-  console.log(verification.checked);
-  const newEntry = {
-    entry_date: data.get("entry_date"),
-    wake_feel: data.get("wake_feel"),
-    accomplish: data.get("accomplish"),
-    main_entry: data.get("main_entry"),
-    tomorrow: data.get("tomorrow"),
-  };
-  console.log(newEntry);
-  fetch("/api/entries", {
-    headers: {
-      "Content-Type": "application/json",
-    },
-    method: "POST",
-    body: JSON.stringify(newEntry),
-  });
+  if (verification.checked) {
+    const data = new FormData(e.target);
+    console.log(verification.checked);
+    const newEntry = {
+      entry_date: data.get("entry_date"),
+      wake_feel: data.get("wake_feel"),
+      accomplish: data.get("accomplish"),
+      main_entry: data.get("main_entry"),
+      tomorrow: data.get("tomorrow"),
+    };
+    console.log(newEntry);
+    fetch("/api/entries", {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify(newEntry),
+    });
+  } else {
+    e.preventDefault();
+    const message = document.querySelector(".verification-message");
+    message.innerText = "You forgot that you like cats...";
+  }
 });
